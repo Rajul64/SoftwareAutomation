184eeb94ee9a49756642333bf7e0e466e73cacd4
Rajul64 <Rajulg70@gmail.com>
2024-03-15 20:27:12 +0530

    deleted all project

--- MyFirstJavaProject/src/test/EnumConstants.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public enum EnumConstants {
-	
-	Sunday,
-	Monday
-
-}
--- MyFirstJavaProject/src/test/MyConstants.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-public class MyConstants {
-	
-	public static final int Long_Wait = 30;
-
-}
